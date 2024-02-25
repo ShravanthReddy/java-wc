960696d54517947ad9c13d8f87c40410f62c9797
Shravanth Reddy Reddy <shrvnthrdy@gmail.com>
2024-02-24 19:51:24 -0500

    Added help command to the Wc tool and also added a test file to check if the tool is working manually.

--- src/Wc.java
+++ src/Wc.java
@@ -2,8 +2,8 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.HashSet;
 
-public class wc {
-    static String[] definedCommands = {"-c", "-l", "-w", "-f", "-h"};
+public class Wc {
+    static String[] definedCommands = {"-c", "-l", "-w", "-f", "-m", "-h"};
     static HashSet<String> commands = new HashSet<>();
     static HashSet<String> filePaths = new HashSet<>();
 
@@ -37,6 +37,11 @@ public class wc {
             System.out.println("Wc: Unable to read input");
         }
 
+        if (args.length == 1 && commands.contains("-h")) {
+            String helpOutput = "java Wc [flags] [filename] \n\nFlags:\n  -h,\thelp\n  -c,\tOutputs the numbers of bytes in the file\n  -l,\tOutputs the numbers of lines in the file\n  -w,\tOutputs the numbers of words in the file\n  -f\tOutputs the freq of each word in the file\n  -m,\tOutputs the most repeated word and it's count";
+            printToConsole(helpOutput);
+        }
+
     }
 
     static boolean argsProcessor(String[] args) {
@@ -45,12 +50,12 @@ public class wc {
             if (arg.startsWith("-")) {
                 if (Arrays.asList(definedCommands).contains(arg)) {
                     if (fileFound) {
-                        System.out.println("Wc: illegal input format\nusage: java Wc [-clwfh] [file ...]");
+                        System.out.println("Wc: illegal input format\nusage: java Wc [-clwfm] [file ...]");
                         return false;
                     }
                     commands.add(arg);
                 } else {
-                    System.out.println("Wc: illegal option - " + arg + "\nusage: java Wc [-clwfh] [file ...]");
+                    System.out.println("Wc: illegal option - " + arg + "\nusage: java Wc [-clwfm] [file ...]");
                     return false;
                 }
             } else if (arg.lastIndexOf(".") != -1) {
@@ -85,7 +90,7 @@ public class wc {
                 case "-w" -> "W:" + wcOperations.getCountOfWords() + "\n";
                 case "-l" -> "L:" + wcOperations.getCountOfLines() + "\n";
                 case "-f" -> wcOperations.getWordHashMap() + "\n";
-                case "-h" -> wcOperations.getHighestWordCount() + "\n";
+                case "-m" -> wcOperations.getHighestWordCount() + "\n";
                 default -> "";
             });
         }
